989b41d37a592802519c68206af9e4aaf0cb54d2
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2022-01-05 10:43:18 +0100

    Add filter

--- src/index.js
+++ src/index.js
@@ -12,3 +12,16 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+server.get("/users", (req, res) => {
+  const filterByGender = req.query.gender;
+  const filteredmovies = movies.filter((movie) => {
+    return filterByGender === ""
+      ? movie
+      : movie.gender.includes(filterByGender);
+  });
+  const response = {
+    success: true,
+    movies: filteredmovies,
+  };
+  res.json(response);
+});
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,16 +1,22 @@
 // login
 
-const getMoviesFromApi = () => {
+const getMoviesFromApi = (params) => {
+  console.log(params);
+  const queryParams = `?gender=${params.gender}`;
+  console.log(queryParams);
   console.log("Se están pidiendo las películas de la app");
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch("//locahost:4000/users/movies")
+  return fetch("//localhost:4000/users/" + queryParams, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       return data;
     });
 };
-
 const objToExport = {
   getMoviesFromApi: getMoviesFromApi,
 };
