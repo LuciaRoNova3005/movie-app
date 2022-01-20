a5d9d8e2087efd370ed4a7c2d034189045d3065c
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2022-01-20 11:33:29 +0100

    npm install ejs

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ server.get("/users", (req, res) => {
     );
   }
   // sort
-
   const sort = req.query.sort === "asc" ? "desc" : "asc";
   const sortedMovies = filteredByGenderMovies.sort((movieA, movieB) => {
     if (sort === "asc") {
